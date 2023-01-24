903ede865a997a08bfce9d2a17179ff722e912b9
lamasgergo <lamasgergo33@gmail.com>
2023-01-24 18:12:42 +0100

    App local updated

--- config/app_local.example.php
+++ config/app_local.example.php
@@ -5,6 +5,10 @@
  * Note: It is not recommended to commit files with credentials such as app_local.php
  * into source code version control.
  */
+
+use Cake\Database\Connection;
+use Cake\Database\Driver\Mysql;
+
 return [
     /*
      * Debug Level:
@@ -25,7 +29,7 @@ return [
      *   You should treat it as extremely sensitive data.
      */
     'Security' => [
-        'salt' => env('SECURITY_SALT', '__SALT__'),
+        'salt' => env('SECURITY_SALT', 'eb80b8cf15a9c531c694bb6338f9824bcdb9c663bc78ff18da4db7c1a1af7b39'),
     ],
 
     /*
@@ -36,41 +40,57 @@ return [
      */
     'Datasources' => [
         'default' => [
-            'host' => 'localhost',
-            /*
-             * CakePHP will use the default DB port based on the driver selected
-             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
-             * the following line and set the port accordingly
-             */
-            //'port' => 'non_standard_port_number',
+            'className' => Connection::class,
+            'driver' => Mysql::class,
+            'persistent' => false,
+            'host' => env('DATABASE_HOST', '127.0.0.1'),
+            'port' => env('DATABASE_PORT', 3306),
+            'username' => env('DATABASE_USERNAME', 'root'),
+            'password' => env('DATABASE_PASSWORD', ''),
+            'database' => env('DATABASE_DATABASE', false),
+            'encoding' => env('DATABASE_ENCODING', 'utf8'),
+            'timezone' => 'UTC',
+            'flags' => [],
+            'cacheMetadata' => true,
+            'log' => env('DATABASE_LOG_ENABLED', false),
 
-            'username' => 'my_app',
-            'password' => 'secret',
-
-            'database' => 'my_app',
-            /*
-             * If not using the default 'public' schema with the PostgreSQL driver
-             * set it here.
+            /**
+             * Set identifier quoting to true if you are using reserved words or
+             * special characters in your table or column names. Enabling this
+             * setting will result in queries built using the Query Builder having
+             * identifiers quoted when creating SQL. It should be noted that this
+             * decreases performance because each query needs to be traversed and
+             * manipulated before being executed.
              */
-            //'schema' => 'myapp',
+            'quoteIdentifiers' => true,
 
-            /*
-             * You can use a DSN string to set the entire configuration
+            /**
+             * During development, if using MySQL < 5.6, uncommenting the
+             * following line could boost the speed at which schema metadata is
+             * fetched from the database. It can also be set directly with the
+             * mysql configuration directive 'innodb_stats_on_metadata = 0'
+             * which is the recommended value in production environments
              */
+            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+
             'url' => env('DATABASE_URL', null),
         ],
-
-        /*
-         * The test connection is used during the test suite.
-         */
-        'test' => [
-            'host' => 'localhost',
-            //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'test_myapp',
-            //'schema' => 'myapp',
-            'url' => env('DATABASE_TEST_URL', 'sqlite://127.0.0.1/tests.sqlite'),
+        'old' => [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
+            'persistent' => FALSE,
+            'host' => env('DATABASE_HOST_OLD', '127.0.0.1'),
+            'port' => env('DATABASE_PORT_OLD', 3306),
+            'username' => env('DATABASE_USERNAME_OLD', 'root'),
+            'password' => env('DATABASE_PASSWORD_OLD', ''),
+            'database' => env('DATABASE_DATABASE_OLD', 'old'),
+            'encoding' => 'utf8',
+            'timezone' => 'UTC',
+            'flags' => [],
+            'cacheMetadata' => TRUE,
+            'log' => TRUE,
+            'quoteIdentifiers' => FALSE,
+            'url' => env('DATABASE_URL', NULL),
         ],
     ],
 
