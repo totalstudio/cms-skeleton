b84ee1682b708709911c15b15a0e570ff0307f71
lamasgergo <gergo@lamas.hu>
2023-09-29 08:26:52 +0200

    BAsic functionality into skeleton added

--- webroot/js/scripts.js
+++ webroot/js/scripts.js
@@ -5,5 +5,34 @@ jQuery.event.special.touchstart = {
 };
 
 $(document).ready(function(){
+    if ($(this).scrollTop() > 140) {
+        $('header').addClass('scrolled');
+    } else if ($(this).scrollTop() < 100) {
+        $('header').removeClass('scrolled');
+    }
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 140) {
+            $('header').addClass('scrolled');
+        } else if ($(this).scrollTop() < 100) {
+            $('header').removeClass('scrolled');
+        }
+    });
 
+    $('.box a, .nav  a, .carousel-caption a').on('click', function(){
+        scrollToBlock($(this).attr('href'));
+    });
+
+    scrollToBlock(location.hash);
 });
+
+function scrollToBlock(temp){
+    var count = (temp.match(/#/g) || []).length;
+    // Handler for .ready() called.
+    if(count > 0){
+        var divClass = temp.split("#")[1];
+        if($('.'+divClass).length > 0) {
+            var element = document.querySelector('.' + divClass);
+            element.scrollIntoView({ block: "center" });
+        }
+    }
+}
